82b6be670c8862a58938bfe809ee8e549f711180
xxmehmetali <xxmehmetali@hotmail.com>
2023-02-28 23:38:25 +0300

    lecture1 covered

---
+++ lecture1/python-introduction.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# declaring variables
+number1 = 1
+number2 = 2
+
+string1 = "this is the string 1"
+print(number1)
+print(number2)
+print(string1)
+
+
+# In[8]:
+
+
+num1 = 1
+num2 = 2
+num3 = 3
+my_list = [num1, num2, num3]
+type(my_list)
+
+
+# In[18]:
+
+
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list)
+print(my_list[1:2])
+my_list.append(4)
+my_list.remove(2)
+print(my_list)
+
+
+# In[21]:
+
+
+my_list.reverse()
+print(my_list)
+
+
+# In[31]:
+
+
+my_list.sort()
+print(my_list)
+
+
+# In[34]:
+
+
+for loop_var in range(1,5):
+    print(loop_var)
+
+
+# In[35]:
+
+
+my_list2 = []
+for loop_var in range(1,10):
+    my_list2.append(loop_var)
+print(my_list2)
+
+
+# In[38]:
+
+
+# finding minimum number in a list
+my_list3 = [21, 2, 3, 14, 66, 110, 5, 7, 38]
+maximum = my_list3[0]
+for element in my_list3:
+    if maximum > element: # change ">" with "<" if you want to find maximum
+        maximum = element
+print(maximum)        
+
+
+# In[40]:
+
+
+i = 0
+while(i < 3):
+    print(i*2)
+    i+=1
+
+
+# In[45]:
+
+
+def func1(name = "defaultName", surname = "defaultSurname"):
+    return name + " " + surname
+func1()
+
+
+# In[50]:
+
+
+# lambdas are used to define simple functions
+getPowerOf2 = lambda num : num * num
+print(getPowerOf2(2))
+
+addTwoNum = lambda num1, num2 : num1 + num2
+print(addTwoNum(1,7))
+
+
+# In[59]:
+
+
+dictionary = {"name" : "ali", "surname" : "karadag", "age" : 25}
+type(dictionary)
+print(dictionary.values())
+print(dictionary.keys())
+if("age" in dictionary.keys() and dictionary["age"] > 20):
+    print("bigger than 20")
+
